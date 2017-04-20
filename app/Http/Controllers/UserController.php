81d70ec432e3441aa2b1d9d80e5502bc6f7a25a0
suiyuan <wg91@gzhxyc.com>
2017-04-20 15:28:43 +0800

    创建项目

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/4/20
+ * Time: 14:14
+ */
+
+namespace App\Http\Controllers;
+
+
+class UserController extends Controller
+{
+    public function showIndex($id)
+    {
+        return 1234567;
+    }
+}
